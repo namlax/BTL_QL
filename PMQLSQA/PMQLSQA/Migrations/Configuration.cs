50f9ef91864caded450ce84c58ff82af2fd0e0f7
Nam La <namla2000k@gmail.com>
2021-12-07 07:14:50 +0700

    done

---
+++ PMQLSQA/PMQLSQA/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PMQLSQA.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PMQLSQA.Models.PMQLSQADbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PMQLSQA.Models.PMQLSQADbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
